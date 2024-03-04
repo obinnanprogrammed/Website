ac5c9162ee01c472589d2347e2dfd4bbde98b40d
obinnanprogrammed <obinnanwakwue10@gmail.com>
2024-03-04 09:30:51 -0600

    is my website gonna work?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website to be developed soon!
+          Website to be developed soon! Check back for details.
         </p>
         <a
           className="App-link"
