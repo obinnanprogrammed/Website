0141abc5d6a8846f3afb2dd014b332cbf3891197
Obinna Nwakwue <obinnanwakwue10@gmail.com>
2024-05-16 16:06:23 -0500

    added my website as a project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import Header from './Header.js';
 import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
 import ProjectComp from './ProjectComp.js';
+import website from '../pictures/websitemay2024.png';
 import scaleDict from '../pictures/scale-dict.png';
 import swipeTern from '../pictures/swipetern.png';
 import npcroom from '../pictures/npcroom.png';
@@ -11,6 +12,9 @@ import jpmcInternship from '../pictures/jpmc-internship.png';
 
 function Projects() {
     const projLinks = {
+        website: [
+            { url: "https://www.github.com/obinnanprogrammed/Website", text: "Github" }
+        ],
         scaleDictionary: [
             { url: "https://www.github.com/obinnanprogrammed/ScaleDictionary", text: "Github" }
         ],
@@ -31,9 +35,10 @@ function Projects() {
     }
 
     const projStacks = {
+        website: [ "React", "MaterialUI", "EmailJS" ],
         scaleDictionary: [ "React Native", "MusicXML", "Expo" ],
         swipeTern: [ "React Native", "Firebase", "Expo" ],
-        npcroom: [ "React", "Java", "Spring Boot", "MongoDB" ],
+        npcroom: [ "React", "MaterialUI", "Java", "Spring Boot", "MongoDB" ],
         revRewards: [ "React", "ChakraUI", "Django", "Tesseract" ],
         jpmcInternship: [ "React", "Java", "Spring Boot", "Apache Cassandra" ]
     }
@@ -42,6 +47,11 @@ function Projects() {
             <Header />
             <Typography m={2} style={{ color: "white", textAlign: "center", margin: '3%' }} variant="h3">projects!</Typography>
             <Grid container justifyContent="space-around" alignItems="center" spacing={2} style={{ margin: '0 auto', maxWidth: '100%' }}>
+                <Grid item lg={4} md={6} xs={12}>
+                    <ProjectComp image={website} title="My Website" 
+                    description="My personal website, where my projects, accomplishments, and aspirations are stored." 
+                    links={projLinks.website} techStack={projStacks.website} />
+                </Grid>
                 <Grid item lg={4} md={6} xs={12}>
                     <ProjectComp image={scaleDict} title="Scale Dictionary" 
                     description="An interactive scale dictionary for beginning musicians. Currently in the works." 
@@ -58,13 +68,13 @@ function Projects() {
                     full stack development using React, Java, Spring Boot, and MongoDB by creating a simple,
                     but evolving NPC chat room." links={projLinks.npcroom} techStack={projStacks.npcroom}/>
                 </Grid>
-                <Grid item lg={6} md={6} xs={12}>
+                <Grid item lg={4} md={6} xs={12}>
                     <ProjectComp image={revRewards} title="Reveille Rewards" 
                     description="Winner of Best Small Business Hack at HowdyHack 2023. A web app that allows
                     users to scan receipts from small businesses and earn redeemable tokens." 
                     links={projLinks.revRewards} techStack={projStacks.revRewards}/>
                 </Grid>
-                <Grid item lg={6} md={12} xs={12}>
+                <Grid item lg={4} md={6} xs={12}>
                     <ProjectComp image={jpmcInternship} title="JPMC Internship Project" 
                     description="The resulting project from a summer (June-Aug. 2022) internship with 
                     JP Morgan Chase. Retrieves data stored on an Apache Cassandra database and displays it
