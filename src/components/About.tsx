08c21936abad3cf99e97e8d03aca631890a495ac
Obinna Nwakwue <obinnanwakwue10@gmail.com>
2024-06-10 15:26:36 -0500

    edited about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import Typography from '@mui/material/Typography';
-import { Box } from '@mui/material';
+import { Box, Link } from '@mui/material';
 import Header from './Header';
 import ImageCarousel from './ImageCarousel';
 
@@ -22,19 +22,25 @@ const About: FC<AboutProps> = () => {
                     <Typography variant="body1">
                         Currently, he is involved in Aggie Coding Club, where he is in his second semester as a
                         Project Manager. His project, NPCRoom, teaches students about full stack web development 
-                        using React, Java, Spring Boot, and MongoDB. He is also a member of Texas A&M Computing Society
+                        using React, Java, Spring Boot, and MongoDB. He is also a member of the Texas A&M Computing Society
                         (Texas A&M's student chapter of the Assocation for Computing Machinery) as well as the Texas A&M
                         Chapter of the National Society of Black Engineers.
                     </Typography>
                     <Typography variant="body1">
-                        In addition to NPCRoom, Obinna has worked on several other software development projects.
-                        Notably, he helped develop Reveille Rewards, the winner of the "Best Small Business Hack"
-                        at HowdyHack 2023. The web app rewards users for shopping at small businesses. He is currently
-                        working on two more notable projects. He is looking to expand his development skillset and knowledge of the
-                        field of computer science. He is also open to internships and co-op opportunities.
+                        Obinna enjoys developing projects that help other people out. Notably, he helped develop 
+                        Reveille Rewards, the winner of the "Best Small Business Hack" at HowdyHack 2023. 
+                        The web application rewards users for shopping at small businesses. Currently, he is working
+                        on his ScaleDictionary project, which aims to provide beginning musicians an interactive
+                        dictionary that can help them learn and practice musical scales.
                     </Typography>
                     <Typography variant="body1">
-                        Outside of computer science, Obinna enjoys music and Pokémon.
+                        Outside of computer science, Obinna enjoys playing and composing music. He has composed several
+                        pieces for trombone ensemble, brass ensemble, and concert band. He is also a member of the
+                        Texas A&M University Trombone Choir.
+                    </Typography>
+                    <Typography variant="body1">
+                        Obinna is open to internships, co-op opportunities, and any opportunity to develop his skillset
+                        and knowledge of computer science and music technology. He can be contacted {<Link href="/links">here</Link>}.
                     </Typography>
                 </Box>
                 {/*<img m={2} src={proPic} alt="Obinna Nwakwue" width="40%" height="80%" /> */}
